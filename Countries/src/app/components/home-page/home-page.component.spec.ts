5a6c62e57d76f7f192629fab679dca5c8f4950ff
nadeen <nadeenshaheen.shahin@gmail.com>
2021-03-21 17:53:28 +0200

    Home page component

---
+++ Countries/src/app/components/home-page/home-page.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HomePageComponent } from './home-page.component';
+
+describe('HomePageComponent', () => {
+  let component: HomePageComponent;
+  let fixture: ComponentFixture<HomePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], 
+      declarations: [ HomePageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(HomePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("testing title",()=>{
+    expect(component.componentName).toBe("Home")
+  })
+});
---
+++ Countries/src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,78 @@
+import { Component, OnInit } from '@angular/core';
+import { Country } from 'src/app/CountryApi';
+import { CountryApiService } from '../../srevices/country-api.service';
+import { Observable } from 'rxjs';
+
+
+
+const Allregions= ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+ componentName="Home"
+
+   search?:Country[];
+  searchFilter?:string;
+  regionFilter?: string;
+  AllOptions = Allregions;
+ 
+  countries$?:Observable<Country[]>;
+  
+  constructor(private countryApiService: CountryApiService) { }
+
+  ngOnInit(): void {
+    //  this.countries$ = this.countryApiService.getAlCountries()
+   this.countryApiService.getAlCountries()
+     .subscribe((countries)=>{
+      this.search=countries;
+    
+    })
+  
+  }
+  get countries(){
+    return this.search
+    ?this.search.filter((country)=>
+    this.searchFilter ? country.name.toLocaleLowerCase()
+    .includes(this.searchFilter.toLowerCase()): country)
+
+    .filter((country)=>
+    this.regionFilter
+    ?country.region
+    .includes(
+      this.regionFilter
+    )
+    :country
+    )
+    :this.search
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
