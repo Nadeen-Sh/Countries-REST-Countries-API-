e8f4eef42e1f258e16f94f6489d336fb9a54fea1
nadeen <nadeenshaheen.shahin@gmail.com>
2021-03-25 20:35:26 +0200

    add more unit test #7

--- Countries/src/app/components/subComponents/navbar/navbar.component.spec.ts
+++ Countries/src/app/components/subComponents/navbar/navbar.component.spec.ts
@@ -2,15 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NavbarComponent } from './navbar.component';
 import { RouterTestingModule } from "@angular/router/testing";
+import { Router } from '@angular/router';
 
 describe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
+  let routerSpy = {navigate: jasmine.createSpy('navigate')}; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule,RouterTestingModule], 
-      declarations: [ NavbarComponent ]
+      declarations: [ NavbarComponent ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ]
     })
     .compileComponents();
   });
@@ -25,9 +30,16 @@ describe('NavbarComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  
+  // 3.  Html element testing
   it("testing html element",()=>{
     const data=fixture.nativeElement;
     expect(data.querySelector(".title").textContent).toContain("Around the world")
   })
+
+  // 4. Navigate to home testing
+  it(`should navigate to home`, () => {
+    component.goHome();
+    expect (routerSpy.navigate).toHaveBeenCalledWith(['/']);
+ });
+
 });
